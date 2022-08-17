162b4e57a35a02436207e1941056a97e4d5a8497
Ifeyinwa Unoka <unokaifeyinwaruth@gmail.com>
2022-08-17 10:22:12 +0100

    first commit

---
+++ app/src/main/java/com/example/calorietrackercomposeapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.calorietrackercomposeapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calorietrackercomposeapplication.ui.theme.CalorieTrackerComposeApplicationTheme
+import com.example.onboading_presentation.welcome.WelcomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalorieTrackerComposeApplicationTheme {
+                WelcomeScreen()
+            }
+        }
+    }
+}
